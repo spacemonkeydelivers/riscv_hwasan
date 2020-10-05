92233440cc69fd5fb62f75b0d1ef2e049f2fc930
Alex White <space.monkey.delivers@gmail.com>
2020-10-05 09:05:49 +0300

    Buildroot updated

--- setup_git.sh
+++ setup_git.sh
@@ -9,6 +9,8 @@ git submodule update --init --recursive
 # switch QEMU to proper branch
 cd ${QEMU_SRC_DIR}
 git checkout riscv_pm_qemu_5.1
+git submodule init
+git submodule update
 cd ${ROOT_DIR}
 
 # switch Linux to proper branch
@@ -23,5 +25,5 @@ cd ${ROOT_DIR}
 
 # switch LLVM to proper branch
 cd ${BUILDROOT_SRC_DIR}
-git checkout 2020.05.x
+git checkout 0e09875f0947c32aca19a351123bde0bd9026aff
 cd ${ROOT_DIR}
