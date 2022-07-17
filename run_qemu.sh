a47de612e77339c7f06b6e97d03da625e89714df
Alexey Baturo <space.monkey.delivers@gmail.com>
2022-07-17 12:57:21 +0300

    Update qemu running script

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Add monitor to connect to qemu: -monitor unix:<unix socket path>,server,nowait
+# Connect to the monitor with: socat -,echo=0,icanon=0 unix-connect:<unix socket path>
+
+# Redirect qemu output to telnet socket instead of stdio with: -serial telnet::<port>,server,nowait
+# Connect to telnet socket with telnet localhost <port>
+
 ROOT_DIR="$(pwd)"
-MAC=$(whoami | md5sum | sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/')
+MAC=$(echo "$(whoami)| $(pwd)" | md5sum | sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/')
 
 source scripts/env.sh
 
 QEMU_BIN=${INSTALL_DIR}/bin/qemu-system-riscv64
 
-sudo ${QEMU_BIN} \
+sudo -E ${QEMU_BIN} \
     -nographic \
     -machine virt -cpu rv64,x-j=on \
     -kernel ${LINUX_BUILD_DIR}/arch/riscv/boot/Image \
@@ -16,4 +22,4 @@ sudo ${QEMU_BIN} \
     -device virtio-blk-device,drive=hd0 \
     -m 1024M \
     -device virtio-net-device,netdev=net0,mac=${MAC} \
-    -netdev tap,id=net0,script=./scripts/add_to_bridge.sh,downscript=./scripts/del_from_bridge.sh
+    -netdev tap,id=net0,script=./scripts/add_to_bridge.sh,downscript=./scripts/del_from_bridge.sh 
